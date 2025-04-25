b501eb5f90f9c0262d8e41e7a4607f8c6b3f067c
soundarpandi <shankar.soundarpandi@accenture.com>
2025-04-25 18:55:41 +0530

    prod

---
+++ Installation_Scripts/Instalization _scripts.sh
@@ -0,0 +1,7 @@
+cd Infra-automation
+chmod 777 Install_AWS_CLI.sh Install_Ansible.sh Install_Jenkins.sh Install_Terraform.sh startup_script.sh
+./Install_AWS_CLI.sh
+./Install_Ansible.sh
+./Install_Jenkins.sh
+./Install_Terraform.sh
+
