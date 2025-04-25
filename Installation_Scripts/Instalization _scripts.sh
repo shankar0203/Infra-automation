d6b93c2601c8369b7cae4fa5389c07e99f324d2f
soundarpandi <shankar.soundarpandi@accenture.com>
2025-04-25 23:26:07 +0530

    prod-3

--- Installation_Scripts/Instalization _scripts.sh
+++ Installation_Scripts/Instalization _scripts.sh
@@ -5,6 +5,7 @@ chmod 777 Install_AWS_CLI.sh Install_Ansible.sh Install_Jenkins.sh Install_Terra
 ./Install_Terraform.sh
 ./Install_Jenkins.sh
 chmod 664 /etc/ansible/hosts
+aws s3 cp s3://ml-test-env/Jenkins-key.pem /home/ubuntu
 chmod 400 /home/ubuntu/Jenkins-key.pem
 
 
---
+++ cmds/permission.sh
@@ -0,0 +1,2 @@
+chmod 664 /etc/ansible/hosts
+chmod 400 /home/ubuntu/Jenkins-key.pem
