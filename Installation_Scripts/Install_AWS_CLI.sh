09ea2c4c657f2e1d68660b7b55be29d8d7dedeb1
soundarpandi <shankar.soundarpandi@accenture.com>
2025-04-25 18:46:21 +0530

    commit

--- Installation_Scripts/Install_AWS_CLI.sh
+++ Installation_Scripts/Install_AWS_CLI.sh
@@ -1,3 +1,5 @@
+# Install AWS_CLI On Ubuntu
+sudo apt update && sudo apt install unzip -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
 unzip awscliv2.zip && \
 sudo ./aws/install && \
