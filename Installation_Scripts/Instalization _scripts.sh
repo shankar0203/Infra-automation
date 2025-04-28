27ed8301c8521a4ffdb764798c0b94f0598b98d9
soundarpandi <shankar.soundarpandi@accenture.com>
2025-04-28 11:50:43 +0530

    prod

--- Installation_Scripts/Instalization _scripts.sh
+++ Installation_Scripts/Instalization _scripts.sh
@@ -5,10 +5,13 @@ chmod 777 Install_AWS_CLI.sh Install_Ansible.sh Install_Jenkins.sh Install_Terra
 ./Install_Terraform.sh
 ./Install_Jenkins.sh
 chmod 777 /etc/ansible
-chmod 664 /etc/ansible/hosts
+chmod 777 /etc/ansible/hosts
 touch hosts.bak
 chmod 400 /etc/ansible/hosts.bak
 aws s3 cp s3://ml-test-env/Jenkins-key.pem /home/ubuntu
 chmod 400 /home/ubuntu/Jenkins-key.pem
-
-
+sudo cp /home/ubuntu/Jenkins-key.pem /var/lib/jenkins/
+sudo chown jenkins:jenkins /var/lib/jenkins/Jenkins-key.pem
+sudo chmod 400 /var/lib/jenkins/Jenkins-key.pem
+sudo chown jenkins:jenkins /var/lib/jenkins/Jenkins-key.pem
+sudo chmod 400 /var/lib/jenkins/Jenkins-key.pem
--- cmds/cmds.sh
+++ cmds/cmds.sh
@@ -4,36 +4,18 @@ cd Terraform_Scripts
 # Terraform cmds
 terraform init
 # terraform destroy -auto-approve
-# terraform plan -var-file="terraform.tfvars"
-# terraform apply -auto-approve -var-file="terraform.tfvars"
+terraform plan -var-file="terraform.tfvars"
+terraform apply -auto-approve -var-file="terraform.tfvars"
 
 # Get the public IP from Terraform output
 PUBLIC_IP=$(terraform output -raw ec2_public_ip)
-echo "$PUBLIC_IP" > /tmp/ec2_instance_ip.txt
+echo "$PUBLIC_IP" > /tmp/ec2_instance_ip.ini
 echo "Public IP of created EC2: $PUBLIC_IP"
 
-# Ansible inventory update
-HOST_FILE="/etc/ansible/hosts"
-GROUP_NAME="webservers"
-ANSIBLE_USER="ubuntu"
-KEY_PATH="/home/ubuntu/Jenkins-key.pem"
-ansible_host="$PUBLIC_IP ansible_user=$ANSIBLE_USER ansible_ssh_private_key_file=$KEY_PATH"
-
-# Backup before edit
-cp $HOST_FILE ${HOST_FILE}.bak
-sudo chmod o+w /etc/ansible/hosts
-sudo chmod o+w /etc/ansible/
-
-
-# Check if group exists, if not, add it
-if ! grep -q "^\[$GROUP_NAME\]" "$HOST_FILE"; then
-  echo -e "\n[$GROUP_NAME]" >> "$HOST_FILE"
-fi
-
-# Add full host entry under the group if not already present
-if ! awk "/^\[$GROUP_NAME\]/ {found=1} /^\[/ {found=0} found && /$PUBLIC_IP/ {exit 1} END {if (found) exit 0; else exit 1}" "$HOST_FILE"; then
-  sed -i "/^\[$GROUP_NAME\]/a $ansible_host" "$HOST_FILE"
-  echo "✅ Host entry added: $ansible_host"
-else
-  echo "ℹ️ Host entry for $PUBLIC_IP already exists under group [$GROUP_NAME]"
-fi
+echo "[webservers]" > /etc/ansible/hosts
+echo "$PUBLIC_IP ansible_ssh_user=ec2-user ansible_ssh_private_key_file=/var/lib/jenkins/Jenkins-key.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'" >> /etc/ansible/hosts
+cd ..
+cd Ansible_Scripts
+# Run the playbook with the generated inventory file
+ansible all -m ping
+ansible-playbook -i /etc/ansible/inventory.ini /var/lib/jenkins/workspace/Test/Ansible_Scripts/playbook.yml
